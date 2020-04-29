18cc86fdbd604eef1ac3a013cfed465b87d1129e
ssoper <ssoper@usgs.gov>
2020-04-29 15:33:46 -0500

    put original indexes back in place and ensure they are created when dropping creating new partitions

--- liquibase/changeLogs/capture/functions/pruneTimeSeriesData/pruneTimeSeriesData.sql
+++ liquibase/changeLogs/capture/functions/pruneTimeSeriesData/pruneTimeSeriesData.sql
@@ -35,22 +35,28 @@ begin
 
 	/* time_series_approvals */
 	execute format('create unique index if not exists time_series_approvals_%s_time_series_approvals_id on %I.time_series_approvals_%s (time_series_approvals_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
+	execute format('create index if not exists time_series_approvals_%s_json_data_id on %I.time_series_approvals_%s (json_data_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
 
 	/* time_series_gap_tolerances */
 	execute format('create unique index if not exists time_series_gap_tolerances_%s_time_series_gap_tolerances_id on %I.time_series_gap_tolerances_%s (time_series_gap_tolerances_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
+	execute format('create index if not exists time_series_gap_tolerances_%s_json_data_id on %I.time_series_gap_tolerances_%s (json_data_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
 
 	/* time_series_grades */
 	execute format('create unique index if not exists time_series_grades_%s_time_series_grades_id on %I.time_series_grades_%s (time_series_grades_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
+	execute format('create index if not exists time_series_grades_%s_json_data_id on %I.time_series_grades_%s (json_data_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
 
 	/* time_series_header_info */
 	execute format('create unique index if not exists time_series_header_info_%s_time_series_header_info_id on %I.time_series_header_info_%s (time_series_header_info_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
 	execute format('create unique index if not exists time_series_header_info_%s_ak on %I.time_series_header_info_%s (json_data_id, time_series_unique_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
+	execute format('create index if not exists time_series_header_info_%s_time_series_unique_id on %I.time_series_header_info_%s (time_series_unique_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
 
 	/* time_series_interpolation_types */
 	execute format('create unique index if not exists time_series_interpolation_types_%s_time_series_interpolation_types_id on %I.time_series_interpolation_types_%s (time_series_interpolation_types_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
+	execute format('create index if not exists time_series_interpolation_types_%s_json_data_id on %I.time_series_interpolation_types_%s (json_data_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
 
 	/* time_series_methods */
 	execute format('create unique index if not exists time_series_methods_%s_time_series_methods_id on %I.time_series_methods_%s (time_series_methods_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
+	execute format('create index if not exists time_series_methods_%s_json_data_id on %I.time_series_methods_%s (json_data_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
 
 	/* time_series_points */
 	execute format('create unique index if not exists time_series_points_%s_time_series_points_id on %I.time_series_points_%s (time_series_points_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
@@ -58,6 +64,7 @@ begin
 
 	/* time_series_qualifiers */
 	execute format('create unique index if not exists time_series_qualifiers_%s_time_series_qualifiers_id on %I.time_series_qualifiers_%s (time_series_qualifiers_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
+	execute format('create index if not exists time_series_qualifiers_%s_json_data_id on %I.time_series_qualifiers_%s (json_data_id)', month_name, '${AQTS_SCHEMA_NAME}', month_name);
 
 end
 $$
